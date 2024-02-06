ac2702f8c0c0658d48a0ab644f2870ff77b27cb8
StephenOmitoki <117896175+StephenOmitoki@users.noreply.github.com>
2024-02-05 19:39:37 -0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,188 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package person;
+
+/**
+ *
+ * @author stephenomitoki
+ */
+public class Person {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: " + getName();
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Peter");
+        Student student = new Student("Susan");
+        Employee employee = new Employee("Eva");
+        Faculty faculty = new Faculty("Frank");
+        Staff staff = new Staff("Shane");
+
+        System.out.println(person);
+        System.out.println(student);
+        System.out.println(employee);
+        System.out.println(faculty);
+        System.out.println(staff);
+    }
+
+}
+
+class Student extends Person {
+
+    private String classStatus;
+
+    public Student(String name) {
+        super(name);
+    }
+
+    public String getClassStatus() {
+        return classStatus;
+    }
+
+    public void setClassStatus(String classStatus) {
+        this.classStatus = classStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: " + getName();
+    }
+}
+
+class Employee extends Person {
+
+    private String office;
+    private double salary;
+    private String dateHired;
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDateHired() {
+        return dateHired;
+    }
+
+    public void setDateHired(String dateHired) {
+        this.dateHired = dateHired;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: " + getName();
+    }
+}
+
+class Faculty extends Employee {
+
+    private String officeHours;
+    private String rank;
+
+    public Faculty(String name) {
+        super(name);
+    }
+
+    public String getOfficeHours() {
+        return officeHours;
+    }
+
+    public void setOfficeHours(String officeHours) {
+        this.officeHours = officeHours;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty: " + getName();
+    }
+}
+
+class Staff extends Employee {
+
+    private String title;
+
+    public Staff(String name) {
+        super(name);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff: " + getName();
+    }
+}
